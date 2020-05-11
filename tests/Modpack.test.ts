3117acb64e4522f5d0db10e9226d77b38424e2a1
Boris Liao <borisliaos@gmail.com>
2020-05-11 18:24:39 -0400

    Added more descriptive test

--- tests/Modpack.test.ts
+++ tests/Modpack.test.ts
@@ -2,8 +2,8 @@ import { determineType } from '../src/main';
 import { expect } from 'chai';
 
 describe('determineType()', function() {
-  it('should determine the right modpack type from zip', function() {
+  it('should determine type:\'twitch\' from sample.zip', function() {
     let pack = determineType("tests/sample.zip");
     expect(pack).equal("twitch");
-  }); 
+  });
 });
