0f1264893c9ba14a26dfecd612c94173f9561cf9
Boris Liao <borisliaos@gmail.com>
2020-05-12 00:14:04 -0400

    getURL helper function

--- src/Twitch.ts
+++ src/Twitch.ts
@@ -1,16 +1,53 @@
 import Modpack from './Modpack';
 import * as fs from 'fs';
-import * as http from 'http';
-import * as path from 'path';
+import * as https from 'https';
+import * as Path from 'path';
 
-const API = (projectID: String, fileID: String) => 'https://addons-ecs.forgesvc.net/api/v2/addon/'+projectID+'/file/'+fileID+'/download-url'
+const API = (projectID: string, fileID: string) => 'https://addons-ecs.forgesvc.net/api/v2/addon/'+projectID+'/file/'+fileID+'/download-url'
 
 class Twitch extends Modpack{
-  readonly manifest : JSON;
+  readonly manifest;
 
-  constructor(file: string, type: string, author: string, version: string, manifest: JSON) {
+  constructor(file: string, type: string, author: string, version: string, manifest) {
       super(file, type, author, version);
       this.manifest = manifest;
+  }
+
+  private getURL(URL: string): string {
+    https.get(URL, (res) => {
+      const { statusCode } = res;
+      const contentType = res.headers['content-type'];
+
+      let error;
+      if (statusCode !== 200) {
+        error = new Error('Request Failed.\n' +
+                          `Status Code: ${statusCode}`);
+      } else if (!/^text\/plain/.test(contentType)) {
+        error = new Error('Invalid content-type.\n' +
+                          `Expected text/plain but received ${contentType}`);
+      }    
+
+      if (error) {
+        console.error(error.message);
+        // Consume response data to free up memory
+        res.resume();
+        return;
+      }
+
+      res.setEncoding('utf8');
+      let rawData = '';
+      res.on('data', (chunk) => { rawData += chunk; });
+      res.on('end', () => {
+        try {
+          return rawData;
+        } catch (e) {
+          console.error(e.message);
+        }
+      });
+    }).on('error', (e) => {
+      console.error(`Got error: ${e.message}`);
+    });
+    return;
   }
 
   /**
@@ -19,7 +56,21 @@ class Twitch extends Modpack{
    * @param path Location to save mods folder to
    */
   download(path: string){
+    let savePath: Path.ParsedPath;
     
+    // Assure path will contain a mods folder
+    if(Path.basename(path) == "mods"){
+      savePath = Path.parse(path);
+    }else{
+      savePath = Path.parse(Path.join(path,'mods'));
+    }
+
+    let files = this.manifest.files;
+
+    for(let i = 0; i < files.length; i++){
+      let downloadURL = API(files[i].projectID,files[i].fileID);
+        console.log(this.getURL(downloadURL))
+    }
   }
 
   /**
