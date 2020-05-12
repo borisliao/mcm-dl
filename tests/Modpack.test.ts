ab495643ad4af802f7a554974cf1b7f205fde9c5
Boris Liao <borisliaos@gmail.com>
2020-05-11 23:02:32 -0400

    createModpack() test

--- tests/Modpack.test.ts
+++ tests/Modpack.test.ts
@@ -1,9 +1,24 @@
-import { determineType } from '../src/main';
+import * as main from '../src/main';
 import { expect } from 'chai';
+import Twitch from '../src/Twitch'
+import * as AdmZip from 'adm-zip';
 
 describe('determineType()', function() {
   it('should determine type:\'twitch\' from sample.zip', function() {
-    let pack = determineType("tests/sample.zip");
+    let pack = main.determineType("tests/sample.zip");
     expect(pack).equal("twitch");
+  });
+});
+
+describe('createModpack()', function() {
+  it('should return twitch pack from sample.zip', function() {
+    let pack = main.createModpack("tests/sample.zip");
+    let zip = new AdmZip(pack.file);
+    let manifest = JSON.parse(zip.readAsText('manifest.json'));
+    let x = new Twitch("tests/sample.zip",'twitch',"3upman","4.0.1", manifest);
+    expect(pack.file).equal('tests/sample.zip');
+    expect(pack.type).equal('twitch');
+    expect(pack.version).equal('4.0.1');
+    expect(pack.author).equal('3upman');
   });
 });
