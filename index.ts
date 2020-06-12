5600aa7c421f40f580f711675c5bfd6efe7ddd0d
Boris Liao <borisliaos@gmail.com>
2020-06-11 20:53:32 -0400

    index.ts exports all classes

---
+++ index.ts
@@ -0,0 +1,5 @@
+import {determineType, createModpack} from './src/main';
+import Modpack from './src/Modpack';
+import Twitch from './src/Twitch';
+
+export default {determineType, createModpack, Modpack, Twitch}
